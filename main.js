ed3d44905be8d8aad385f349739f13e49c7bc747
Fathul Irfaan Abdillah <abdillahfathu@gmail.com>
2022-08-23 06:36:32 +0700

    Added gsap animation timeline

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-gsap.to('.anime', {
-  duration: 2,
-  x: 300,
-  backgroundColor: 'pink',
-  borderRadius: '20%',
-  border: '5px solid black',
-  ease: 'bounce.in',
-})
+const tl = gsap.timeline({ repeat: 3, yoyo: true })
+
+tl.from('.anime', { duration: 1, opacity: 0, scale: 0.3, ease: 'bounce' })
+
+tl.to(
+  '.box',
+  {
+    duration: 2,
+    rotation: 360,
+    x: 100,
+    stagger: 0.3,
+  },
+  '+=1'
+)
+
+// tween.pause()
+
+// document
+//   .querySelector('.pause-btn')
+//   .addEventListener('click', () => tween.pause())
+
+// document
+//   .querySelector('.play-btn')
+//   .addEventListener('click', () => tween.play())
+
+// document
+//   .querySelector('.seek-btn')
+//   .addEventListener('click', () => tween.seek(2))
+
+// document
+//   .querySelector('.progress-btn')
+//   .addEventListener('click', () => tween.progress(0.5))
